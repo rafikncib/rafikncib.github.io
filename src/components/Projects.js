2efce3fcef93ceb5f64fc06d2854c53d002b7b04
rafikncib <rafikncib@gmail.com>
2023-01-06 19:04:41 +0100

    change animation fadinRight

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -3,10 +3,10 @@ import Project from "./Project";
 const Projects=(props)=>{
     return(
         <div id="projectsContent">
-            <div className="container pt-2">
-                <div className="row gy-2 animate__animated animate__fadeInUp">
+            <div className="container pt-3">
+                <div className="row gy-2 animate__animated animate__fadeInRight">
                     {
-                        props.projects.map((elem,key)=>{
+                        props.projects?.map((elem,key)=>{
                             return (
                                 <div className="col-12 col-md-4" key={key}>
                                     <Project project={elem}/>
--- src/components/Works.js
+++ src/components/Works.js
@@ -17,8 +17,8 @@ const Works=(props)=>{
                     <div className="hstack gap-3 justify-content-center">
                         <button type="button" className={`btn ${buildedProjects!=='projects'?'bg-black':' btn-danger'}`} onClick={()=>handleClick("projects")} id="all">ALL</button>
                         <button type="button" className={`btn ${buildedProjects!=='web_dev'?'bg-black':' btn-danger'}`} onClick={()=>handleClick("web_dev")}>Web developmet</button>
-                        <button type="button" className={`btn ${buildedProjects!=='reactJs'?'bg-black':' btn-danger'}`} onClick={()=>handleClick("reactJs")}>Experience</button>
-                        <button type="button" className={`btn ${buildedProjects!=='python'?'bg-black':' btn-danger'}`} onClick={()=>handleClick("python")}>Experience</button>
+                        <button type="button" className={`btn ${buildedProjects!=='reactJs'?'bg-black':' btn-danger'}`} onClick={()=>handleClick("reactJs")}>reactJs</button>
+                        <button type="button" className={`btn ${buildedProjects!=='python'?'bg-black':' btn-danger'}`} onClick={()=>handleClick("python")}>python</button>
                     </div>
                 </header>
                 {
